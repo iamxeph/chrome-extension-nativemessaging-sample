f973b6d3b04c91f1d548397bbab258118139dad5
Xeph <iamxeph@gmail.com>
2020-08-07 23:51:55 +0900

    Add dotnet-console-test source

---
+++ NativeHost/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_console_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
